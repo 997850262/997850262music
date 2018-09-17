05f0cb7c789a8e43285bb3d4232b851e50af4dde
997850262 <997850262@qq.com>
2018-09-17 10:45:04 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import configureStore from './store/configureStore';
+/* eslint-disable */
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
